2aaa2a6f7e69b627202dbe560207c7bd6dc601ef
Lorraine <lm2194@pleasantonusd.net>
2025-03-17 09:14:34 -0700

    i made a change in VScode

--- main.java
+++ main.java
@@ -2,4 +2,7 @@
 System.out.println("Hello!");
 System.out.println("test");
 
-System.out.println("this is an update")
+System.out.println("this is an update");
+
+System.out.println("update from VScode");
+System.out.println("another update?");
